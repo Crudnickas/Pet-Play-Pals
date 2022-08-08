e5c1258ca320f8c46f99c250ba068c44332be7df
Kelsie Wenger <wenger.kelsie@gmail.com>
2022-08-08 14:15:21 -0400

    adding Pet

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+      <!-- <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp; -->
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+body {
+background-color:#F0EEE4;
+color: #878357;
+font-family: 'Poppins', sans-serif;
+
+}
+</style>
+
---
+++ vue/src/components/PetRegister.vue
@@ -0,0 +1,59 @@
+<template>
+<form class="form-register" @submit.prevent="register">
+     <label for="petName" class="sr-only">Pet Name: </label>
+      <input
+        type="text"
+        id="petName"
+        class="form-control"
+        placeholder="Pet Name"
+        v-model="pet.name"
+      />
+      <br>
+      <!-- THIS NEEDS TO BE added with the right information -->
+       <label for="petAge" class="sr-only">Pet Age: </label>
+        <select name="petAge" id="petAge" v-model="pet.age">
+        <option disabled value = ""> Please Select one </option>
+        <option value="puppy">Puppy (under 1 Year)</option>
+        <option value="adult">Adult (1 to 9 years)</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+        
+    </select>
+    <br>
+          <label for="petSize" class="sr-only">Pet Size: </label>
+        <select name="petSize" id="petSize" v-model="pet.size">
+        <option disabled value = ""> Please Select one </option>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+        
+    </select>
+
+
+</form>
+  
+</template>
+
+<script>
+export default {
+name: 'pet-register',
+data(){
+    return{
+        pet:{
+            name:"",
+            age:"",
+            size:"",
+            breed:"",
+            temperament:"",
+            energy:"",
+            bio:""
+        }
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
