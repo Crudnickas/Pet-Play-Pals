a3fd39a1b588891facba795476c04e9bf9c5850a
Tom Montague <tmontague4@gmail.com>
2022-08-09 09:37:17 -0400

    updated PlayDate Model

--- dotnet/Capstone/Models/PlayDate.cs
+++ dotnet/Capstone/Models/PlayDate.cs
@@ -7,5 +7,18 @@ namespace Capstone.Models
 {
     public class PlayDate
     {
+        public string Location { get; set; }
+
+        public int ScheduleID { get; set; }
+
+        public string PlayDateName { get; set; }
+
+        public string PlayDateAddress { get; set; }
+
+        public DateTime ScheduledDate { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
     }
 }
